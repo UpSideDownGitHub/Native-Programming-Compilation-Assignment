eaa6bca325c6347790a5e26bde7f19be528d1586
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2022-11-09 16:22:46 +0000

    Shaders & Others
    sdkjgfqkwajshfbkj\shygbvfe

--- Native_Compilation_Project/Assets/Camera/Scripts/CameraShake.cs
+++ Native_Compilation_Project/Assets/Camera/Scripts/CameraShake.cs
@@ -7,7 +7,8 @@ public class CameraShake : MonoBehaviour
 {
     public Camera cam;
     public bool alreadyShaking;
-    public Vector3 initPosition;
+    //public Vector3 initPosition;
+    public float initFOV;
     public float timeToWait;
 
     public void Start()
@@ -26,17 +27,20 @@ public class CameraShake : MonoBehaviour
     private IEnumerator _ShakingCamera(float length, float hardness)
     {
         alreadyShaking = true;
-        initPosition = cam.transform.position;
+        //initPosition = cam.transform.position;
+        initFOV = cam.fieldOfView;
 
         float time = 0f;
         while (time < length)
         {
-            cam.transform.localPosition = initPosition + (Vector3)Random.insideUnitCircle * hardness;
+            cam.fieldOfView = cam.fieldOfView + Random.Range(-1f, 1f) * hardness;
+            //cam.transform.position = cam.transform.position + (Vector3)Random.insideUnitCircle * hardness;
             time += timeToWait;
             yield return new WaitForSeconds(timeToWait);
         }
 
-        cam.transform.position = initPosition;
+        //cam.transform.position = initPosition;
+        cam.fieldOfView = initFOV;
         alreadyShaking = false;
     }
 }
